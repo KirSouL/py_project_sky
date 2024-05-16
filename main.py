20cb07b90cf36530c6ff6c0e393d9927e6a665ce
KirSouL <Sokil-Ki-So@yandex.ru>
2024-05-16 23:25:57 +0300

    add update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils_funcs import sum_number
+from utils_funcs import subtraction_number
 
 input_one, input_two = input('Введите число a - '), input('Введите число b - ')
 
 print(sum_number(input_one, input_two))
+print(subtraction_number(input_one, input_two))
